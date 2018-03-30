ab1ff3364bbbc01c6da83ef2632cc8d48da47fbc
Achilleas Yfantis <achilleasein@gmail.com>
2018-03-30 11:36:22 +0300

    Update i2c_arduino_side.ino

--- i2c_arduino_side.ino
+++ i2c_arduino_side.ino
@@ -5,29 +5,29 @@ int number = 0;
 int state = 0;
 
 void setup() {
-pinMode(13, OUTPUT);
-Serial.begin(9600); // start serial for output
-// initialize i2c as slave
-Wire.begin(SLAVE_ADDRESS);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
 
-// define callbacks for i2c communication
-Wire.onReceive(receiveData);
-Wire.onRequest(sendData);
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
 
-Serial.println("Ready!");
+  Serial.println("Ready!");
 }
 
 void loop() {
-delay(100);
+  delay(100);
 }
 
 // callback for received data
 void receiveData(int byteCount){
 
 while(Wire.available()) {
-number = Wire.read();
-Serial.print("data received: ");
-Serial.println(number);
+  number = Wire.read();
+  Serial.print("data received: ");
+  Serial.println(number);
 
 if (number == 1){
 
